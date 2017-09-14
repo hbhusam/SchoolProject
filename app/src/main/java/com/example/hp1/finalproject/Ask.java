5850a7a7a9a3a152486b01d40785b2bb51da5b93
Hp1 <hu_sam_2000@hotmail.com>
2017-09-14 10:58:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hp1/finalproject/Ask.java
@@ -0,0 +1,71 @@
+package com.example.hp1.finalproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class Ask extends AppCompatActivity implements View.OnClickListener,AdapterView.OnItemLongClickListener {
+    Button btadd,btclear;
+    EditText et1;
+    ListView mails;
+    ArrayList<String> emails = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask);
+        btadd=(Button)findViewById(R.id.bt1);
+        btclear=(Button)findViewById(R.id.bt2);
+        mails=(ListView)findViewById(R.id.emails);
+        et1=(EditText)findViewById(R.id.EMail);
+        btadd.setOnClickListener(this);
+        btclear.setOnClickListener(this);
+        emails.add("How To Get A girlFriend?");
+        emails.add("Is My Mind Big?");
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, emails);
+        mails.setAdapter(adapter);
+        mails.setOnItemLongClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==btadd){
+            emails.add(et1.getText().toString());
+            adapter.notifyDataSetChanged();
+
+        }
+        if(v==btclear){
+            et1.setText("");
+
+
+        }
+    }
+
+
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent email = new Intent(Intent.ACTION_SEND);
+
+        email.putExtra(Intent.EXTRA_EMAIL, new String[]{emails.get(position).toString()});
+        email.putExtra(Intent.EXTRA_SUBJECT,"Hello" );
+
+        email.putExtra(Intent.EXTRA_TEXT,"Byee");
+
+
+        email.setType("message/rfc822");
+
+        startActivity(Intent.createChooser(email, "Choose an Email client :"));
+        return false;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/hp1/finalproject/Home.java
@@ -0,0 +1,34 @@
+package com.example.hp1.finalproject;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class Home  extends AppCompatActivity implements View.OnClickListener {
+Button btrelationship, btask ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btrelationship=(Button)findViewById(R.id.btcouple);
+        btask=(Button)findViewById(R.id.btask);
+btrelationship.setOnClickListener(this);
+        btask.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+if (v==btrelationship){
+            Intent i = new Intent(this, Relationship.class);
+            startActivity(i);
+        }
+        if (v==btask){
+            Intent i = new Intent(this, Ask.class);
+            startActivity(i);
+        }
+    }
+}
